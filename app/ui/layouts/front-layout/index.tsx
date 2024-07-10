5c1e417290775804b014d6a745b90d41baa499f1
Valerian <valerian.purnomo@inticorp.tech>
2024-07-10 11:30:26 +0700

    Update responsive on login page

--- app/ui/layouts/front-layout/index.tsx
+++ app/ui/layouts/front-layout/index.tsx
@@ -5,10 +5,10 @@ const FrontLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     <div className=" h-screen w-screen">
       <div className="flex flex-row space-x-2 items-center">
         <div
-          className="pl-4 w-1/2 h-screen bg-center rounded-r-xl"
+          className="pl-4 w-1/2 h-screen bg-center rounded-r-xl max-md:hidden"
           style={{ backgroundImage: "url(./assets/bg-login.jpg)" }}
         />
-        <div className="w-1/2 p-16">{children}</div>
+        <div className="w-1/2 p-16 max-md:w-full">{children}</div>
       </div>
     </div>
   );
