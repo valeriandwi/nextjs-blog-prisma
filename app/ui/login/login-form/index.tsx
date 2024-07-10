02ccb8ca2120d42257d904c1c3d16899389af38c
Valerian <valerian.purnomo@inticorp.tech>
2024-07-10 15:54:56 +0700

    Change credential is using email instead username

--- app/ui/login/login-form/index.tsx
+++ app/ui/login/login-form/index.tsx
@@ -18,7 +18,7 @@ const LoginForm = () => {
         >
           <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
         </svg>
-        <input type="text" className="grow" placeholder="Username" />
+        <input type="email" className="grow" placeholder="Email" />
       </label>
       <label className="input input-bordered flex items-center gap-4">
         <svg
